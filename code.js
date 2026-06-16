0c7eb594075b3412bbc4592ce116ace43d908645
Cameron Kinnear <cmkin402@gmail.com>
2026-06-16 14:26:16 -0400

    semi-functional javascript. need src fixed

--- code.js
+++ code.js
@@ -9,7 +9,6 @@ for (let i = 0; i < buttons.length; i++) {
     console.log("add event listener");
     buttons[i].addEventListener("click", () => {
         buttons[i].classList.toggle("selected");
-        UpdateBeacon(buttons[i]);
         // Add to queue to keep track of oldest selected button
 
         // If element already in queue
@@ -17,6 +16,7 @@ for (let i = 0; i < buttons.length; i++) {
             console.log("spliced");
             const index = iconQueue.indexOf(buttons[i]);
             iconQueue.splice(index, 1);
+            UpdateBeacon();
             return;
         }
 
@@ -28,10 +28,31 @@ for (let i = 0; i < buttons.length; i++) {
 
         // Add button to queue
         iconQueue.push(buttons[i]);
+        UpdateBeacon();
         console.log(iconQueue);
     })
 }
 
-function UpdateBeacon(button) {
+// Updates the webpage to display the currently selected beacon icons
+function UpdateBeacon() {
+    let lenQueue = iconQueue.length;
 
+    const selectedIcons = document.getElementsByClassName("beacon-icon");
+    console.log(selectedIcons);
+    let lenSelected = selectedIcons.length;
+
+    // Remove all the icons from the website to be replaced
+    for (let i = 0; i < lenSelected; i++) {
+        selectedIcons[i].remove();
+    }
+
+    // Add all the icons from the queue back to the page
+    for (let i = 0; i < lenQueue; i++) {
+        let newBeaconIcon = document.createElement("img");
+        newBeaconIcon.src = iconQueue[i].getAttribute('data-src');
+        console.log(iconQueue[i].src);
+        newBeaconIcon.className = "beacon-icon";
+        document.querySelector(".beacon-display").appendChild(newBeaconIcon);
+        
+    }
 }
